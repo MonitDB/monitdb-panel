86ba0aef0c7eaf64b89981906c597ac5b374b7dc
Gerson Lima <gersonthiago.lima@gmail.com>
2022-08-31 23:20:49 +0100

    chore: remove useless useState on header component

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,6 +1,6 @@
 import cn from 'classnames'
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import React from 'react'
 
 import Link from '~/components/link'
 import DatabasesSvg from '~/icons/databases.svg'
