f469d70037e82c88ccd39decd88fb67cea8fe228
Gerson Lima <gersonthiago.lima@gmail.com>
2022-12-08 09:47:13 +0000

    chore(checkbox): add support to defaultValue and onChange native component

--- components/form/checkbox/checkbox.jsx
+++ components/form/checkbox/checkbox.jsx
@@ -1,14 +1,36 @@
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import classNames from 'classnames'
-import React from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 
 import styles from './checkbox.module.css'
 
-const Checkbox = ({ className, ...properties }) => {
+const Checkbox = ({ className, defaultValue, onChange, ...properties }) => {
+  const elementReference = useRef()
+
+  const handleChange = useCallback(
+    (event) => {
+      const checked = event.target.checked
+
+      onChange(checked)
+    },
+    [onChange]
+  )
+
+  useEffect(() => {
+    if (defaultValue) {
+      elementReference.current.checked = defaultValue
+    }
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div className={classNames(styles.checkbox, className)}>
-      <input type="checkbox" {...properties} />
+      <input
+        ref={elementReference}
+        type="checkbox"
+        onChange={handleChange}
+        {...properties}
+      />
       <i>
         <FontAwesomeIcon icon={faCheck} />
       </i>
