74a1ed4bc3bbb27ea01592bde20501ef40cefdf4
Gerson Lima <gersonthiago.lima@gmail.com>
2022-12-08 00:05:54 +0000

    chore(service-servers): update paths and add create, update and delete functions

--- services/servers.js
+++ services/servers.js
@@ -1,7 +1,7 @@
 import clientApi from '~/utils/client-api'
 
 export const getServers = async () => {
-  return clientApi().get(`/server`)
+  return clientApi().get(`/api/server`)
 }
 
 export const getServerMetrics = async ({ id }) => {
@@ -17,16 +17,36 @@ export const getEnvironments = async () => {
 }
 
 export const addServer = async (values) => {
-  return clientApi().post(`/server`, {
-    Idtypeserver: values.serverType,
-    Idtypeserverconection: values.connection,
-    Idtypeserverenvironment: values.environment,
-    Servername: values.name,
-    ServerDescription: values.description,
-    ServerHost: values.host,
-    ServerUser: values.user,
-    ServerPassword: values.password,
-    ServerPort: values.port,
-    Serverenable: values.status,
+  return clientApi().post(`/api/server`, {
+    idtypeserver: values.serverType,
+    idtypeserverconnection: values.connection,
+    idtypeserverenvironment: values.environment,
+    servername: values.name,
+    serverdescription: values.description,
+    serverhost: values.host,
+    serveruser: values.user,
+    serverpassword: values.password,
+    serverport: values.port,
+    // serverenable: values.status,
   })
+}
+
+export const updateServer = async (values) => {
+  return clientApi().put(`/api/server`, {
+    id: values.id,
+    idtypeserver: values.serverType,
+    idtypeserverconnection: values.connection,
+    idtypeserverenvironment: values.environment,
+    servername: values.name,
+    serverdescription: values.description,
+    serverhost: values.host,
+    serveruser: values.user,
+    serverpassword: values.password,
+    serverport: values.port,
+    serverenable: values.status,
+  })
+}
+
+export const deleteServer = async (id) => {
+  return clientApi().delete(`/api/server/${id}`)
 }
