1f1841cd4eff2e69bd59e10eeca50c7f1bdc3e74
Gerson Lima <gersonthiago.lima@gmail.com>
2022-12-19 09:12:05 +0000

    chore(gtm): remove gtm package

--- helpers/main.jsx
+++ helpers/main.jsx
@@ -1,6 +1,5 @@
 import Router from 'next/router'
-import { useEffect, useState } from 'react'
-import TagManager from 'react-gtm-module'
+import { useState } from 'react'
 
 import Contexts from '~/contexts/index'
 
@@ -14,14 +13,6 @@ const Main = ({ children }) => {
   Router.events.on('routeChangeComplete', () => {
     setIsLoadingPage(false)
   })
-
-  useEffect(() => {
-    // How to create a history event change trigger
-    // https://morganfeeney.com/how-to/integrate-google-tag-manager-with-next-js#create-a-history-event-change-trigger
-    TagManager.initialize({
-      gtmId: process.env.gtmId,
-    })
-  }, [])
 
   return (
     <Contexts>
