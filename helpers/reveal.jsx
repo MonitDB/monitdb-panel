507e05af19024fbf4cfe2a78cbf2384e06ad628e
Gerson Lima <gersonthiago.lima@gmail.com>
2023-01-11 10:02:26 +0000

    refactor(reveal): add timer to detect when is active to observe height

--- helpers/reveal.jsx
+++ helpers/reveal.jsx
@@ -1,14 +1,31 @@
 import classNames from 'classnames'
-import React, { useEffect, useRef } from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 
 const Reveal = ({ active, children }) => {
   const elementReference = useRef(null)
+  const timerReference = useRef(null)
 
-  useEffect(() => {
+  const updateHeight = useCallback(() => {
     elementReference.current.style.height = active
       ? `${elementReference.current.scrollHeight}px`
       : `0px`
   }, [active])
+
+  useEffect(() => {
+    updateHeight()
+  }, [updateHeight, active])
+
+  useEffect(() => {
+    active
+      ? (timerReference.current = setInterval(updateHeight, 500))
+      : clearInterval(timerReference.current)
+  }, [updateHeight, active])
+
+  useEffect(() => {
+    return () => {
+      clearInterval(timerReference.current)
+    }
+  }, [])
 
   return (
     <div
