8350e3e065daa5c31ee5707e3a5da53c9beb5616
Geniton Lima <genitom.wd@gmail.com>
2023-01-12 11:15:05 -0300

    feat(component): server metrics

---
+++ components/page/dashboard/server-host-metrics/server-metrics/index.jsx
@@ -0,0 +1,12 @@
+const ServerMetrics = () => {
+  return (
+    <div>
+      <h3>SQL Server metrics</h3>
+      <div>
+        <h4>General</h4>
+      </div>
+    </div>
+  )
+}
+
+export default ServerMetrics
