9d07b44591278ffa86d89a03a6374b212cc43ff7
Geniton Lima <genitom.wd@gmail.com>
2023-01-20 15:49:23 -0300

    feat(dashboard): section id

--- components/page/dashboard/databases/index.jsx
+++ components/page/dashboard/databases/index.jsx
@@ -4,7 +4,7 @@ import Image from '~/components/image'
 
 function Databases() {
   return (
-    <div className="mt-4">
+    <div id="databases" className="mt-4">
       <div className="grid grid-cols-[28px_auto_1fr] gap-2 items-center my-8">
         <Image
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAYCAYAAADpnJ2CAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAaFJREFUeNq8VtFRhDAQTRj+pQOxguMqgA6OErQCxy8+gU++vKvAsQK1A6jAXAdYwWEFuus8ZlYkHgmjO7MDd7B5ydv3QnRRFJFSKqfcUcaUibKHoewpO8rnpml65RiaAE90jZRf1ARauRQEK8A4UteC0BNoYEpBrTOlH45AB8o9+p1RbnCvcI3R5170/UjZcs8Z8PWMUL71DCt7EiAu0QaEuqUbzjsMZs4U5Z5gHFnItiBQA6D9uQp6v1ohsi/RnGiQFgLga+/jr78STQ3h3PsCBq4FtHqm/Qrg7cKysV1bLx+C8kr0NbIo/Ud7mFL20i08FS2gVMF7HbxlFggtwfg7PfPAtoH3oPCaspx5Nie0eGohtgWb+EXM1sCPttmqJQP/ZoscyYMNwvg84zfcX2DVj2ttMRVNBK5t0WH1l6DWecMPPRTKDNxwQnCZYGDODu9oVzuq9MFhtjUGifDFH5ZOdDxZaPEjwyxTQW8CKsa+HvBfOTlyHCdqTYTFNlL52pVSbN7lv21ta8MH0IBmr6OJ9qkSR8tUHDWsX3l5tPwUYADxOZD86Rab3gAAAABJRU5ErkJggg=="
