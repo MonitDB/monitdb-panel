c4907202e71f6a16b02265cbde73ed338d185d54
Gerson Lima <gersonthiago.lima@gmail.com>
2023-01-24 13:59:29 +0000

    fix(dashboard): prettier bugs

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -226,9 +226,10 @@ const DashboardPage = () => {
                   {formattedEnvironments
                     .filter(({ isActive }) => isActive)
                     .map((formattedEnvironment, environmentIndex) => {
-                      const formattedServers = formattedEnvironment.servers.filter(
-                        ({ isActive }) => isActive
-                      )
+                      const formattedServers =
+                        formattedEnvironment.servers.filter(
+                          ({ isActive }) => isActive
+                        )
 
                       if (formattedServers.length === 0) {
                         return ''
@@ -247,7 +248,8 @@ const DashboardPage = () => {
                               {
                                 'border-gray':
                                   formattedEnvironment.isDropdownActive,
-                                'border-gray-light': !formattedEnvironment.isDropdownActive,
+                                'border-gray-light':
+                                  !formattedEnvironment.isDropdownActive,
                               }
                             )}
                             onClick={() => toggleIndexActive(environmentIndex)}
@@ -257,7 +259,8 @@ const DashboardPage = () => {
                               className={classNames(
                                 'transition-all duration-300 ease-in-out transform',
                                 {
-                                  'rotate-180': !formattedEnvironment.isDropdownActive,
+                                  'rotate-180':
+                                    !formattedEnvironment.isDropdownActive,
                                 }
                               )}
                             />
