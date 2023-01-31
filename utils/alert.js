e18bc4cdede7bf7e5949a0152cd1e4503779a7aa
Gerson Lima <gersonthiago.lima@gmail.com>
2023-01-31 08:06:31 +0000

    fix(alert): get correct environment on server

--- utils/alert.js
+++ utils/alert.js
@@ -1,10 +1,10 @@
 export const formatAlert = (alert, { servers, serverEnvironments }) => {
+  const currentServer = servers.find((server) => server.id === alert.idServer)
   return {
     ...alert,
     server: servers.find((server) => server.id === alert.idServer),
     serverEnvironment: serverEnvironments.find(
-      (environment) =>
-        environment.idTypeServerEnvironment === alert.idTypeServerEnvironment
+      (environment) => environment.id === currentServer?.idTypeServerEnvironment
     ),
   }
 }
