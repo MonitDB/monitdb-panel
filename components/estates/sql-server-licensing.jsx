88254a29c164555f85f356087d85598596e5a0b6
Gerson Lima <gersonthiago.lima@gmail.com>
2023-02-01 18:14:17 +0000

    fix: estates and reports env id

--- components/estates/sql-server-licensing.jsx
+++ components/estates/sql-server-licensing.jsx
@@ -31,12 +31,10 @@ const InstalledVersions = ({ tabName }) => {
   const groupsOptions = useMemo(
     () => [
       { value: '', label: 'Todos os grupos' },
-      ...serverEnvironments.map(
-        ({ idTypeServerEnvironment, typeServerEnvironmentName }) => ({
-          value: idTypeServerEnvironment,
-          label: typeServerEnvironmentName,
-        })
-      ),
+      ...serverEnvironments.map(({ id, typeServerEnvironmentName }) => ({
+        value: id,
+        label: typeServerEnvironmentName,
+      })),
     ],
     [serverEnvironments]
   )
