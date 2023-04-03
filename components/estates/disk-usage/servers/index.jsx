e4896408a63b1989caf6a0e19d2c265b55e9f03e
Geniton Lima <genitom.wd@gmail.com>
2023-04-03 11:10:00 -0300

    fix: unused imports removed

--- components/estates/disk-usage/servers/index.jsx
+++ components/estates/disk-usage/servers/index.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable sonarjs/cognitive-complexity */
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
-import { faDatabase } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import classNames from 'classnames'
 import React, { useCallback, useState } from 'react'
@@ -32,95 +31,93 @@ const Servers = ({ environmentServers, diskUsage }) => {
 
   return (
     <div className="p-3 pb-0 space-y-3">
-      {environmentServers.map(
-        ({ id, serverName, typeServerEnvironmentName }, index) => {
-          const filteredDiskUsage = []
+      {environmentServers.map(({ id, serverName }, index) => {
+        const filteredDiskUsage = []
 
-          for (let disk of diskUsage) {
-            if (id === disk.ServerId) {
-              filteredDiskUsage.push({
-                ...disk,
-                serverName,
-              })
-            }
+        for (let disk of diskUsage) {
+          if (id === disk.ServerId) {
+            filteredDiskUsage.push({
+              ...disk,
+              serverName,
+            })
           }
+        }
 
-          if (filteredDiskUsage.length === 0) {
-            return ''
-          }
-          return (
-            <div key={`environment-server-${index}`}>
-              <button
-                type="button"
-                className={classNames(
-                  `w-full py-2 px-4 bg-white border space-x-4
+        if (filteredDiskUsage.length === 0) {
+          return ''
+        }
+        return (
+          <div key={`environment-server-${index}`}>
+            <button
+              type="button"
+              className={classNames(
+                `w-full py-2 px-4 bg-white border space-x-4
                         rounded-sm font-bold text-left text-sm lg:hover:border-gray`,
+                {
+                  'border-gray': serverExpandedIndices.has(index),
+                  'border-gray-light': !serverExpandedIndices.has(index),
+                }
+              )}
+              onClick={() => handleServerExpandedIndices(index)}
+            >
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                className={classNames(
+                  'transition-all duration-300 ease-in-out transform',
                   {
-                    'border-gray': serverExpandedIndices.has(index),
-                    'border-gray-light': !serverExpandedIndices.has(index),
+                    'rotate-180': serverExpandedIndices.has(index),
                   }
                 )}
-                onClick={() => handleServerExpandedIndices(index)}
-              >
-                <FontAwesomeIcon
-                  icon={faChevronDown}
-                  className={classNames(
-                    'transition-all duration-300 ease-in-out transform',
-                    {
-                      'rotate-180': serverExpandedIndices.has(index),
-                    }
-                  )}
-                />
-                <span>{serverName}</span>
-              </button>
-              <Reveal active={serverExpandedIndices.has(index)}>
-                <div className="w-full mt-3 prose max-w-full prose-p:m-0 prose-th:text-center prose-td:text-center prose-td:align-top prose-th:whitespace-nowrap prose-th:border-b-4 prose-headings:m-0 prose-td:whitespace-nowrap prose-td:text-ellipsis prose-td:overflow-hidden bg-white p-4 space-y-2">
-                  <table className="m-0">
-                    <thead>
-                      <tr>
-                        <th>Disco</th>
-                        <th>Espaço usado</th>
-                        <th>Capacidade</th>
-                        <th>Porcentagem usada</th>
-                        <th>Espaço livre</th>
-                        <th>Espaço livre porcentagem</th>
-                      </tr>
-                    </thead>
+              />
+              <span>{serverName}</span>
+            </button>
+            <Reveal active={serverExpandedIndices.has(index)}>
+              <div className="w-full mt-3 prose max-w-full prose-p:m-0 prose-th:text-center prose-td:text-center prose-td:align-top prose-th:whitespace-nowrap prose-th:border-b-4 prose-headings:m-0 prose-td:whitespace-nowrap prose-td:text-ellipsis prose-td:overflow-hidden bg-white p-4 space-y-2">
+                <table className="m-0">
+                  <thead>
+                    <tr>
+                      <th>Disco</th>
+                      <th>Espaço usado</th>
+                      <th>Capacidade</th>
+                      <th>Porcentagem usada</th>
+                      <th>Espaço livre</th>
+                      <th>Espaço livre porcentagem</th>
+                    </tr>
+                  </thead>
 
-                    <tbody>
-                      {filteredDiskUsage.map((disk, index) => (
-                        <tr key={`server-production-${index}`}>
-                          <td>
-                            <p>
-                              <Link
-                                href="/estates/?tab=disk-usage"
-                                className="text-blue no-underline"
-                              >
-                                {disk.Drive ? `${disk.Drive}:` : disk.Volume}
-                              </Link>
-                            </p>
-                          </td>
-                          <td>{disk['Usage(MB)']} MB</td>
-                          <td>{disk['Total(MB)']} MB</td>
-                          <td>
-                            {getPercentage(
-                              disk['Usage(MB)'],
-                              disk['Total(MB)']
-                            ).toFixed(2)}
-                            %
-                          </td>
-                          <td>{disk['Livre(MB)']} MB</td>
-                          <td>{disk['Livre(%)']}%</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </Reveal>
-            </div>
-          )
-        }
-      )}
+                  <tbody>
+                    {filteredDiskUsage.map((disk, index) => (
+                      <tr key={`server-production-${index}`}>
+                        <td>
+                          <p>
+                            <Link
+                              href="/estates/?tab=disk-usage"
+                              className="text-blue no-underline"
+                            >
+                              {disk.Drive ? `${disk.Drive}:` : disk.Volume}
+                            </Link>
+                          </p>
+                        </td>
+                        <td>{disk['Usage(MB)']} MB</td>
+                        <td>{disk['Total(MB)']} MB</td>
+                        <td>
+                          {getPercentage(
+                            disk['Usage(MB)'],
+                            disk['Total(MB)']
+                          ).toFixed(2)}
+                          %
+                        </td>
+                        <td>{disk['Livre(MB)']} MB</td>
+                        <td>{disk['Livre(%)']}%</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </Reveal>
+          </div>
+        )
+      })}
     </div>
   )
 }
