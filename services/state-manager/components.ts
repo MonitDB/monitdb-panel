98ebb4e5d26d836c07ba2d44996e08a723efbd1b
Abimael Albuquerque <abimael.albuquerque@colab.re>
2023-05-26 12:34:29 -0400

    feat: use zustand at components

---
+++ services/state-manager/components.ts
@@ -0,0 +1,32 @@
+import { create } from 'zustand'
+
+import clientApi from '../../utils/client-api'
+import { formatObjectToQuery } from '../../utils/formats'
+
+interface ComponentContext {
+  getComponents: (parameters?: Record<string, any>) => Promise<any>
+  getComponentById: (componentId: string) => Promise<any>
+  updateComponentById: (values: Record<string, any>) => Promise<any>
+  getComponentTypes: (parameters?: Record<string, any>) => Promise<any>
+}
+
+const useComponentContext = create<ComponentContext>((set, get) => ({
+  getComponents: async (parameters = {}) => {
+    return clientApi().get(`/api/component?${formatObjectToQuery(parameters)}`)
+  },
+  getComponentById: async (componentId) => {
+    return clientApi().get(`/api/component/${componentId}`)
+  },
+  updateComponentById: async (values) => {
+    return clientApi().put(`/api/component`, {
+      ...values,
+    })
+  },
+  getComponentTypes: async (parameters = {}) => {
+    return clientApi().get(
+      `/api/typecomponent?${formatObjectToQuery(parameters)}`
+    )
+  },
+}))
+
+export default useComponentContext
