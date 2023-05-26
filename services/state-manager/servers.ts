c7905fcc2b657f8476ac11d49c84659abcee61e2
Abimael Albuquerque <abimael.albuquerque@colab.re>
2023-05-26 12:27:51 -0400

    feat: created ServerContext

---
+++ services/state-manager/servers.ts
@@ -0,0 +1,61 @@
+import { create } from 'zustand'
+import clientApi from '../../utils/client-api'
+
+interface ServerContext {
+  getServers: () => Promise<any>
+  getServerMetrics: (params: { id: string }) => Promise<any>
+  getTypes: () => Promise<any>
+  getEnvironments: () => Promise<any>
+  addServer: (values: any) => Promise<any>
+  updateServer: (values: any) => Promise<any>
+  deleteServer: (id: string) => Promise<any>
+}
+
+const useServerContext = create<ServerContext>((set, get) => ({
+  getServers: async () => {
+    return clientApi().get(`/api/server`)
+  },
+  getServerMetrics: async ({ id }) => {
+    return clientApi().get(`/api/metrics/${id}`)
+  },
+  getTypes: async () => {
+    return clientApi().get(`/api/typeserver`)
+  },
+  getEnvironments: async () => {
+    return clientApi().get(`/api/typeserverenvironment`)
+  },
+  addServer: async (values) => {
+    return clientApi().post(`/api/server`, {
+      idtypeserver: values.serverType,
+      idtypeserverconnection: values.connection,
+      idtypeserverenvironment: values.environment,
+      servername: values.name,
+      serverdescription: values.description,
+      serverhost: values.host,
+      serveruser: values.user,
+      serverpassword: values.password,
+      serverport: values.port,
+      // serverenable: values.status,
+    })
+  },
+  updateServer: async (values) => {
+    return clientApi().put(`/api/server`, {
+      id: values.id,
+      idtypeserver: values.serverType,
+      idtypeserverconnection: values.connection,
+      idtypeserverenvironment: values.environment,
+      servername: values.name,
+      serverdescription: values.description,
+      serverhost: values.host,
+      serveruser: values.user,
+      serverpassword: values.password,
+      serverport: values.port,
+      serverenable: values.status,
+    })
+  },
+  deleteServer: async (id) => {
+    return clientApi().delete(`/api/server/${id}`)
+  },
+}))
+
+export default useServerContext
