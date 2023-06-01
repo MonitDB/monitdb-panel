6218a57afc2cda3a76d6705ce7fd4e572b5c8cad
Abimael Albuquerque <abimael.albuquerque@colab.re>
2023-06-01 17:19:08 -0400

    feat: improve call the component from API

--- services/state-manager/components.js
+++ services/state-manager/components.js
@@ -3,12 +3,22 @@ import { create } from 'zustand'
 import clientApi from '../../utils/client-api'
 import { formatObjectToQuery } from '../../utils/formats'
 
-const useComponentContext = create(() => ({
+const useComponentContext = create((set) => ({
+  loading: false,
+
   getComponents: async (parameters = {}) => {
     return clientApi().get(`/api/component?${formatObjectToQuery(parameters)}`)
   },
   getComponentById: async (componentId) => {
-    return clientApi().get(`/api/component/${componentId}`)
+    try {
+      set({loading: true})
+      const { data } = clientApi().get(`/api/component/${componentId}`);
+      return data;
+    } catch (error) {
+      console.log(error)
+    } finally {
+      set({ loading: false})
+    }
   },
   updateComponentById: async (values) => {
     return clientApi().put(`/api/component`, {
