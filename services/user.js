cda3cca6c306164cfbfc19840b85a82a87ec8856
Abimael Albuquerque <abimael.albuquerque@colab.re>
2023-10-03 12:24:23 -0300

    fix

--- services/user.js
+++ services/user.js
@@ -1,7 +1,7 @@
 import clientApi from '~/utils/client-api'
 
 export const postLogin = async (data) => {
-  return clientApi().post('/api/user//login', data)
+  return clientApi().post('/api/user/login', data)
 }
 
 export const postTokenValidate = async (token) => {
