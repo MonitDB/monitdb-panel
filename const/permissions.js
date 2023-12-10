964d7bf8fe3f4054d22762870037387f44320688
Abimael Albuquerque <abimael.albuquerque@colab.re>
2023-12-10 20:22:32 -0300

    feat: permission constant

---
+++ const/permissions.js
@@ -0,0 +1,7 @@
+export const permission = {
+    DASHBOARD_PAGE: 3,
+    ALERT_PAGE : 4,
+    REPORT_PAGE : 5,
+    STATES_PAGE: 6,
+    CONFIGURATION_PAGE: 7,
+}
--- pages/configurations/profiles/[id].jsx
+++ pages/configurations/profiles/[id].jsx
@@ -7,18 +7,25 @@ import { useFormik } from 'formik'
 import { useRouter } from 'next/router'
 import { NextSeo } from 'next-seo'
 import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 import * as Yup from 'yup'
 
 import Loading from '~/components/loading/loading'
 import { PageContent, PageHeader, PageWrapper } from '~/components/page'
 import Layout from '~/layouts/default'
-import { getPermissions, getProfileById } from '~/services/permissions'
+import {
+  createProfile,
+  getPermissions,
+  getProfileById,
+  updateProfile,
+} from '~/services/permissions'
 
 const EditProfilePage = () => {
   const router = useRouter()
   const { id: profileId } = router.query
-  const [loading, setLoading] = useState(true)
-  const [permissions, setPermissions] = useState([]) // New state for permissions
+  const [fetching, setIsFetching] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [permissions, setPermissions] = useState([])
 
   const validationSchema = Yup.object({
     roleName: Yup.string().required('Profile Name is required'),
@@ -34,20 +41,26 @@ const EditProfilePage = () => {
     validationSchema,
     onSubmit: async (values) => {
       try {
-        console.log(values)
-        // Add your logic for creating or updating the profile here
+        setLoading(true)
 
-        window.location.href = '/configurations/profiles'
+        if (profileId !== 'new-profile') {
+          updateProfile(profileId, values)
+        } else {
+          createProfile(values)
+        }
+        toast.success('Profile saved successfully')
+        router.push('/configurations/profiles')
       } catch (error) {
-        console.error(error)
-        // Add logic to handle creation or update errors
+        toast.error(error.message)
+      } finally {
+        setLoading(false)
       }
     },
   })
 
   useEffect(() => {
     async function fetchData() {
-      setLoading(true)
+      setIsFetching(true)
       try {
         const { data: permissionsData } = await getPermissions()
         setPermissions(permissionsData)
@@ -59,7 +72,7 @@ const EditProfilePage = () => {
       } catch (error) {
         console.error(error)
       } finally {
-        setLoading(false)
+        setIsFetching(false)
       }
     }
 
@@ -90,18 +103,6 @@ const EditProfilePage = () => {
     )
   }
 
-  if (loading) {
-    return (
-      <Layout>
-        <PageWrapper className="p-8">
-          <PageContent removeSidebarMargin={true}>
-            <Loading />
-          </PageContent>
-        </PageWrapper>
-      </Layout>
-    )
-  }
-
   return (
     <>
       <NextSeo
@@ -112,98 +113,113 @@ const EditProfilePage = () => {
           <PageContent removeSidebarMargin={true}>
             {renderBreadcrumb()}
 
-            <div className="mt-8">
-              <form onSubmit={formik.handleSubmit} className="max-w-xl mx-auto">
-                <div className="mb-4">
-                  <label
-                    htmlFor="profileName"
-                    className="block text-sm font-medium text-gray-600"
-                  >
-                    Profile Name:
-                  </label>
-                  <input
-                    type="text"
-                    id="profileName"
-                    name="roleName"
-                    value={formik.values.roleName}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    className="mt-1 p-2 border rounded-md w-full"
-                  />
-                  {formik.touched.roleName && formik.errors.roleName && (
-                    <div className="text-red-500">{formik.errors.roleName}</div>
-                  )}
-                </div>
-                <div className="mb-4">
-                  <label
-                    htmlFor="profileDescription"
-                    className="block text-sm font-medium text-gray-600"
-                  >
-                    Description:
-                  </label>
-                  <textarea
-                    id="profileDescription"
-                    name="roleDescription"
-                    value={formik.values.roleDescription}
-                    onBlur={formik.handleBlur}
-                    className="mt-1 p-2 border rounded-md w-full"
-                  />
-                  {formik.touched.roleDescription &&
-                    formik.errors.roleDescription && (
+            {fetching && (
+              <div className="flex justify-center items-center w-full min-h-28">
+                <Loading light />
+              </div>
+            )}
+
+            {!fetching && (
+              <div className="mt-8">
+                <form
+                  onSubmit={formik.handleSubmit}
+                  className="max-w-xl mx-auto"
+                >
+                  <div className="mb-4">
+                    <label
+                      htmlFor="roleName"
+                      className="block text-sm font-medium text-gray-600"
+                    >
+                      Profile Name:
+                    </label>
+                    <input
+                      type="text"
+                      id="roleName"
+                      name="roleName"
+                      value={formik.values.roleName}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      className="mt-1 p-2 border rounded-md w-full"
+                    />
+                    {formik.touched.roleName && formik.errors.roleName && (
                       <div className="text-red-500">
-                        {formik.errors.roleDescription}
+                        {formik.errors.roleName}
                       </div>
                     )}
-                </div>
-
-                <div className="mb-4">
-                  <label className="block text-sm font-medium text-gray-600">
-                    Permissions:
-                  </label>
-                  <div className="mt-2">
-                    {permissions.map((permission) => (
-                      <div key={permission.id} className="flex items-center">
-                        <input
-                          type="checkbox"
-                          id={`permission-${permission.id}`}
-                          name={`permissions[${permission.id}]`}
-                          checked={formik.values.permissions?.includes(
-                            permission.id
-                          )}
-                          alt={permission.description}
-                          title={permission.description}
-                          onChange={({ target: { checked } }) => {
-                            const newPermissions = checked
-                              ? [...formik.values.permissions, permission.id]
-                              : formik.values.permissions.filter(
-                                  (id) => id !== permission.id
-                                )
-
-                            formik.setFieldValue('permissions', newPermissions)
-                          }}
-                          className="mr-2"
-                        />
-                        <label
-                          htmlFor={`permission-${permission.id}`}
-                          title={permission.description}
-                        >
-                          {permission.name}
-                        </label>
-                      </div>
-                    ))}
                   </div>
-                </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="roleDescription"
+                      className="block text-sm font-medium text-gray-600"
+                    >
+                      Description:
+                    </label>
+                    <textarea
+                      id="roleDescription"
+                      name="roleDescription"
+                      value={formik.values.roleDescription}
+                      onBlur={formik.handleBlur}
+                      onChange={formik.handleChange}
+                      className="mt-1 p-2 border rounded-md w-full"
+                    />
+                    {formik.touched.roleDescription &&
+                      formik.errors.roleDescription && (
+                        <div className="text-red-500">
+                          {formik.errors.roleDescription}
+                        </div>
+                      )}
+                  </div>
 
-                <button
-                  type="submit"
-                  onClick={formik.submitForm}
-                  className="btn btn--primary"
-                >
-                  <FontAwesomeIcon icon={faSave} className="mr-2" />
-                  Save
-                </button>
-              </form>
-            </div>
+                  <div className="mb-4">
+                    <label className="block text-sm font-medium text-gray-600">
+                      Permissions:
+                    </label>
+                    <div className="mt-2">
+                      {permissions.map((permission) => (
+                        <div key={permission.id} className="flex items-center">
+                          <input
+                            type="checkbox"
+                            id={`permission-${permission.id}`}
+                            name={`permissions[${permission.id}]`}
+                            checked={formik.values.permissions?.includes(
+                              permission.id
+                            )}
+                            alt={permission.description}
+                            title={permission.description}
+                            onChange={({ target: { checked } }) => {
+                              const newPermissions = checked
+                                ? [...formik.values.permissions, permission.id]
+                                : formik.values.permissions.filter(
+                                    (id) => id !== permission.id
+                                  )
+
+                              formik.setFieldValue(
+                                'permissions',
+                                newPermissions
+                              )
+                            }}
+                            className="mr-2"
+                          />
+                          <label
+                            htmlFor={`permission-${permission.id}`}
+                            title={permission.description}
+                          >
+                            {permission.name}
+                          </label>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+
+                  <div className="mb-4 flex justify-end">
+                    <button disabled={loading} className="btn btn--small mr-2">
+                      <FontAwesomeIcon icon={faSave} className="mr-2" />
+                      {loading ? 'Saving...' : 'Save'}
+                    </button>
+                  </div>
+                </form>
+              </div>
+            )}
           </PageContent>
         </PageWrapper>
       </Layout>
