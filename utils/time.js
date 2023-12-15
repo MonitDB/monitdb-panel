4cddc6c810a96cb0260ddb04d958321c47c82561
Abimael Albuquerque <abimael.albuquerque@colab.re>
2023-12-14 21:54:48 -0300

    feat: time helpers

---
+++ utils/time.js
@@ -0,0 +1,16 @@
+export function minutesToHours(minutes) {
+  if (typeof minutes !== 'number' || minutes < 0) {
+    throw new Error('O argumento deve ser um número positivo.');
+  }
+
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = minutes % 60;
+
+  return `${hours}h:${remainingMinutes.toString().padStart(2, '00')}m`;
+}
+
+export function calculateMinutesFromDate(value) {
+    const date = new Date(value)
+
+  return date.getHours() * 60 + date.getMinutes();
+}
