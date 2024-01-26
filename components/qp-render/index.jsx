cfb4452f92335e56992297c346769e85c2623221
Abimael Albuquerque <abimaelr.albu@gmail.com>
2024-01-26 08:50:52 -0300

    feat: QP

---
+++ components/qp-render/index.jsx
@@ -0,0 +1,44 @@
+import 'html-query-plan/css/qp.css'
+
+import React, { useEffect, useRef } from 'react'
+import styled from 'styled-components'
+
+const StyledQueryPlanRendererContainer = styled.div`
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+`
+
+const QueryPlanRenderer = ({ queryPlan }) => {
+  const containerReference = useRef(null)
+
+  useEffect(() => {
+    const loadQP = async () => {
+      const QP = await import('html-query-plan')
+
+      const options = {}
+
+      if (
+        QP &&
+        QP.showPlan &&
+        typeof QP.showPlan === 'function' &&
+        containerReference.current
+      ) {
+        QP.showPlan(containerReference.current, queryPlan, options)
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('QP.showPlan is not available or container is not found')
+      }
+    }
+
+    loadQP()
+  }, [queryPlan])
+
+  return (
+    <StyledQueryPlanRendererContainer>
+      <div ref={containerReference} id="qpRender" />
+    </StyledQueryPlanRendererContainer>
+  )
+}
+
+export default QueryPlanRenderer
