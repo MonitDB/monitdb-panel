b991d189cf7f0a6a8cb3447c9fc766394bc64f9d
Abimael Albuquerque <abimaelr.albu@gmail.com>
2024-09-14 20:03:55 -0300

    feat: integration Service

---
+++ services/integration.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+import { apiV2 } from '~/utils/client-api'
+
+export const listIntegrations = async (page = 1, pageSize = 10) => {
+  try {
+    const response = await apiV2().get('/integration', {
+      params: { page, pageSize },
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error listing integrations:', error)
+  }
+}
+
+export const listAllIntegrations = async () => {
+  try {
+    const response = await apiV2().get('/integration/all', {})
+    return response.data
+  } catch (error) {
+    console.error('Error listing integrations:', error)
+  }
+}
+
+export const createIntegration = async (integrationData) => {
+  try {
+    const response = await apiV2().post('/integration', integrationData)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getIntegration = async (id) => {
+  try {
+    const response = await apiV2().get(`/integration/${id}`)
+    return response.data
+  } catch (error) {
+    console.error(`Error getting integration with ID ${id}:`, error)
+  }
+}
+
+export const execIntegration = async (id) => {
+  try {
+    const response = await apiV2().get(`/integration/${id}/execute`)
+    return response.data
+  } catch (error) {
+    console.error(`Error executing integration with ID ${id}:`, error)
+  }
+}
